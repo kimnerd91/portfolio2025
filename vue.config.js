ceb14b23543385b5333f60c7a6225590e3deccc8
kimnerd <kimnerd91@gmail.com>
2025-01-08 21:39:14 +0900

    rework

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-   publicPath: process.env.NODE_ENV === 'production' ? '/ssbmall/' : '/',
+ publicPath: process.env.NODE_ENV === 'production' ? '/portfolio2025/' : '/'
 });
